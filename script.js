9ebed213f6eafe7dfb19223da1dee2c4ce68e862
A D I T Y A <88902527+aditya9277@users.noreply.github.com>
2023-12-26 15:39:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// script.js
+
 function socialSignUp(provider) {
     alert(`Signing up with ${provider}! (This is a demo, no actual sign-up functionality)`);
 }
 
 document.getElementById('userProfileForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    // Process form data or show a success message
+    
     alert('Profile created! (This is a demo, no actual form submission)');
 });
